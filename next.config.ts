d52a61efb4755f033191c0b01f4cb3b19ebb8a8a
Nathan LeRoy <nleroy917@users.noreply.github.com>
2025-12-19 15:58:12 -0500

    remove outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   // Ensure these packages are not bundled on the server (works with Turbopack)
-  outputFileTracingRoot: path.join(__dirname, '../'),
+  // outputFileTracingRoot: path.join(__dirname, '../'),
   serverExternalPackages: [
     'onnxruntime-node',
     'fastembed',
